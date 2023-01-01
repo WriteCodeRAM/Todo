e67aa20a1a28dd34ced4f5bc16c6838b1b355b4f
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-01-01 15:29:17 -0500

    added ability to add tasks to different projects

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-import { createTaskForm, displayTasks } from './tasks';
+import { displayTasks } from './tasks';
 
 const titleInput = document.getElementById('titleInput');
 const newProjectForm = document.querySelector('.newProjectForm');
@@ -11,19 +11,26 @@ let projects = [];
 
 //creates project
 class Project {
-  constructor(title, task) {
+  constructor(title, tasks) {
     this.title = title;
-    this.task = 'click to add tasks';
+    //thinking of having each project contain an array of objects each representing a single task
+    //clean dishes (default task)
+    this.tasks = [
+      {
+        title: 'clean dishes',
+        dueDate: '12/29/22',
+        priority: 'high',
+        description: 'use extra soap',
+      },
+    ];
   }
 }
 
 //add project to array if valid title
 const addProject = () => {
-  console.log(createTaskForm);
   if (titleInput.value) {
     const newProject = new Project(titleInput.value);
     projects.push(newProject);
-    console.log(projects);
   }
   newProjectForm.classList.add('hide');
 
@@ -38,19 +45,23 @@ const displayProject = () => {
     createP.innerText = projects[i].title;
     projectList.appendChild(createP);
 
-    createP.addEventListener('click', displayTasks(projects[i]));
+    //this should display tasks once new project is created,
+    //problem may be here too
+
+    createP.addEventListener('click', displayTasks(projects[i], i));
     //way to allow user to swap between projects displaying appropriate tasks for each ?
 
+    // this should display tasks for specific project when said project title is clicked
     createP.addEventListener('click', (e) => {
       //this works but feels sketch
       let title = e.target.innerText;
       for (let i = 0; i < projects.length; i++) {
         if (projects[i].title === title) {
-          displayTasks(projects[i]);
+          displayTasks(projects[i], i);
         }
       }
     });
   }
 };
 
-export { addProject, todoList, projectHeader };
+export { addProject, todoList, projectHeader, projects };
--- src/tasks.js
+++ src/tasks.js
@@ -1,71 +1,138 @@
-import { todoList, projectHeader } from './projects';
+import { add } from 'lodash';
+import { todoList, projectHeader, projects } from './projects';
 
-//shows form for new task when user clicks btn
-const createTaskForm = (() => {
-  const taskForm = document.createElement('form');
+class Task {
+  constructor(title, dueDate, description, priority) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+  }
+}
 
-  const addTaskBtn = document.createElement('button');
-  addTaskBtn.classList.add('submitButton');
-  taskForm.classList.add('modal');
-  addTaskBtn.innerText = 'Add Task';
+//get form and its inputs to work properly
+function showForm(project, index) {
+  const form = document.createElement('form');
+  form.innerHTML = `<h2>New Task</h2>
+          
+          
+    <label for="Task Name">*Task Name</label>
+    
+       <input type="text" name="task" id="task" required minlength="2" maxlength="28" value="clean dishes">
+    
+    <label for="dueDate">*Due date</label>
+    
+       <input type="date" name="date" id="date" required>
+       
+    
+       <label for="Description">*Description</label>
+       <input type="text" name="description" id="description" required minlength="1" maxlength="35" value="use extra soap">
+    
+        <label for="priority">*Priority</label>
+        <select name="priority" id="priority"> 
+            <option value="Low">Low</option>
+            <option value="Medium">Medium</option>
+            <option value="High">High</option>
+    </select>
+    
+    
+     <button class='submit' type="button" id="taskSubmit">Create task!</button>`;
 
-  //new task form
-  addTaskBtn.addEventListener('click', () => {
-    todoList.appendChild(createTaskForm.taskForm);
+  form.classList.add('modal');
+  form.classList.remove('hide');
+  todoList.appendChild(form);
+  const taskTitle = document.querySelector('#task');
+  const dateInput = document.querySelector('#date');
+  const priorityInput = document.querySelector('#priority');
+  const descriptionInput = document.querySelector('#description');
+  const taskSubmitBtn = document.querySelector('#taskSubmit');
+
+  taskSubmitBtn.addEventListener('click', () => {
+    console.log('test');
+    addTask(
+      index,
+      taskTitle.value,
+      dateInput.value,
+      priorityInput.value,
+      descriptionInput.value
+    );
+    form.classList.add('hide');
   });
 
-  //   addTaskBtn.addEventListener('click', () => {
-  //     todoList.appendChild(createTaskForm.taskForm);
-  //   });
+  //need to find a way for the index to get called with disp[layTask]
+  taskSubmitBtn.addEventListener('click', () => displayTasks(project, index));
 
-  taskForm.innerHTML = `<h2>New Task</h2>
-          
-          
-      <label for="Task Name">*Task Name</label>
-      
-         <input type="text" name="task" id="task" required minlength="2" maxlength="28" value="clean dishes">
-      
-      <label for="dueDate">*Due date</label>
-      
-         <input type="date" name="date" id="date" required>
-         
-      
-         <label for="Description">*Description</label>
-         <input type="text" name="description" id="description" required minlength="1" maxlength="35" value="use extra soap">
-      
-          <label for="priority">*Priority</label>
-          <select name="priority" id="priority"> 
-              <option value="Low">Low</option>
-              <option value="Medium">Medium</option>
-              <option value="High">High</option>
-      </select>
-      
-      
-       <button class='submit' type="button" id="submit">Create task!</button>`;
+  return { taskTitle, taskSubmitBtn };
+}
 
-  return { taskForm, addTaskBtn };
-})();
+//shot in the dark, give each project index its own addTaskBTtn ?
+// const createAddTaskBtn = () => {
+//   //create addTaskBtn
+//   let buttons = [];
+//   for (let i = 0; i < projects.length; i++) {
+//     const button = document.createElement('button');
+//     button.innerText = 'Add Task';
+//     button.classList.add('newTaskBtn');
+
+//     button.setAttribute('id', i);
+//     buttons.push(button);
+//   }
+//   return buttons;
+// };
 
 //display task for specific project runs when user clicks on the project name in sidebar
-const displayTasks = (project) => {
-  const keys = Object.values(project);
+//could use index parameter when it comes to editing the task maybe
+const displayTasks = (project, index) => {
+  let buttons = [];
 
-  //logic to display each task
-  for (let i = 0; i < keys.length; i++) {
-    if (i == 0) {
-      todoList.innerText = '';
-      projectHeader.innerText = project.title;
-      todoList.appendChild(projectHeader);
-    } else {
-      const createP = document.createElement('p');
-      createP.innerText = keys[i];
-      createP.classList.add('task');
-      todoList.appendChild(createP);
-    }
+  //testing
+  for (let i = 0; i < projects.length; i++) {
+    const button = document.createElement('button');
+    button.innerText = 'Add Task';
+    button.classList.add('newTaskBtn');
+
+    button.setAttribute('id', i);
+    button.addEventListener('click', () => {
+      showForm(project, i);
+    });
+
+    buttons.push(button);
   }
-  todoList.appendChild(createTaskForm.addTaskBtn);
+  //reset todolist
+
+  todoList.innerText = ``;
+  //set project header
+  projectHeader.innerText = project.title;
+  todoList.appendChild(projectHeader);
+
+  //displays task for project
+  project.tasks.forEach((task) => {
+    const createP = document.createElement('p');
+    createP.innerText = task.title;
+    createP.classList.add('task');
+    todoList.appendChild(createP);
+  });
+
+  //how can I get this to work multiple times with index
+  //works when I use buttons[0] for the first project...
+  //I want each project to have its own button
+  todoList.appendChild(buttons[index]);
 };
 
-const addTask = (project) => {};
+//ADD TASK LOGIC
+const addTask = (index, taskName, dueDate, description, priority) => {
+  if (taskName !== '' && dueDate && description && priority) {
+    const newTask = new Task(taskName, dueDate, description, priority);
+    console.log(index);
 
-export { createTaskForm, displayTasks };
+    //this is where things get weird
+    //works the first time, but when trying to add a new task it doesnt bc it cant reference the index anymore
+    projects[index].tasks.push(newTask);
+    displayTasks(projects[index]);
+    // } else {
+    //give an error about an empty field or sum shit can probably used 'required' to avoid this
+  }
+};
+// };
+
+export { displayTasks };
